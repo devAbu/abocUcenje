a81acb18158457401825b6643b74a043c6711413
omar97hassan <37211092+omar97hassan@users.noreply.github.com>
2019-03-12 12:00:45 +0100

    zadatak 1
    valjda sam uradio ono sto si htio :D ako nisam javi mi

---
+++ srni/zadatak1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>zadatak 1</title>
+</head>
+<body>
+	<?php
+		require 'connection.php';
+        //$sql = "SELECT * FROM";
+        //$result = $dbc->query($sql);
+        //$count = $result->num_rows;
+        //if ($count > 0) {
+            //while ($row = $result->fetch_assoc()) {
+              //  echo '<div>' . $row[""] . '</div>';
+            //}
+        //}
+    ?>
+</body>
+</html>
