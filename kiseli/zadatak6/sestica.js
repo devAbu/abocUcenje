d3ebfd76ae71d93304d146208c6164fcded3005f
anes <karic.anes59@gmail.com>
2019-04-10 10:04:46 +0200

    dio samo

---
+++ kiseli/zadatak6/sestica.js
@@ -0,0 +1,18 @@
+function myFunction() {
+
+  alert("Stranica se ucitala");
+   console.log('stranica se ucitala');
+
+}
+
+
+
+function submit1(){
+
+                 var a = document.getElementById("number1").value;
+                  var b = document.getElementById("number2").value;
+                   //var c = parseInt(a) + parseInt(b);
+
+                      document.getElementById('rezultat').value = parseInt(a) + parseInt(b);
+
+                  }
