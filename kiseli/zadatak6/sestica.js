5b9820850faa37fcea388a2087bdd16c83491c88
anes <karic.anes59@gmail.com>
2019-04-11 12:46:23 +0200

    JS

--- kiseli/zadatak6/sestica.js
+++ kiseli/zadatak6/sestica.js
@@ -16,3 +16,56 @@ function submit1(){
                       document.getElementById('rezultat').value = parseInt(a) + parseInt(b);
 
                   }
+
+
+
+
+function f1(){
+
+x=document.frm1.user.value;
+y=document.frm1.pass.value;
+
+
+if(x=="" && y ==""){
+  document.getElementById("eresult").innerHTML = "Niste nista unijeli";
+  return false;
+}
+
+else if(x==""){
+  document.getElementById("eresult").innerHTML = "Ispuni x";
+  return false;
+  }
+
+else if (y==""){
+
+  document.getElementById("eresult").innerHTML = "Ispuni y";
+  return false;
+
+  }
+else if (y=="0"){
+  document.getElementById("eresult").innerHTML = "Unesite tacne godine";
+  return false;
+}
+else{
+
+  document.getElementById("eresult").innerHTML = "<span style='color: green;'>Hvala</span>";
+  /*return true;*/
+
+
+  setTimeout(function(){
+
+           document.getElementById("eresult").style.display = "none";
+          /*document.getElementById("eresult").style.visibility = "hidden".reset();*/
+          /*document.getElementById("eresult").style.display = "none".innerHTML = "<span style='color: green;'>Hvala</span>";*/
+
+           },3000);
+/*return false;*/
+
+
+setTimeout(function(){
+
+        var input = document.getElementById("klix5").reset();/*.style.display = "none";*/
+      },3000);
+
+}
+}
